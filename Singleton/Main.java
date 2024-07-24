8583f6b44b859425f9a616f12d8bce16b192f733
RoaGQino <roaqino@gmail.com>
2024-07-24 16:36:34 +0300

    second task

--- Singleton/Main.java
+++ Singleton/Main.java
@@ -1,7 +1,7 @@
 package Singleton;
 
 public class Main {
-
+//ensure that all parts of your application log to the same place, //maintain consistent logging behavior, and manage resources //efficiently.
  public static void main(String[] args) {
    
      Logger logger = Logger.getInstance();
